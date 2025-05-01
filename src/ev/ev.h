f07e804795f440ce36481c7866fd5b4c393da898
thiagoricardop <t.ricardooliveira08@gmail.com>
2025-05-01 00:50:47 -0400

    Updating rpm and temperature change logic

--- src/ev/ev.h
+++ src/ev/ev.h
@@ -2,6 +2,10 @@
 #ifndef EV_H
 #define EV_H
 
+// Engine Simulation Constants - Valores ajustados para maior realismo
+#define EV_TEMP_INCREASE_RATE 0.05      // Taxa de aumento de temperatura
+#define EV_TEMP_DECREASE_RATE 0.01      // Taxa de diminuição de temperatura
+
 void handle_signal(int sig);
 void init_communication();
 void receive_cmd();
